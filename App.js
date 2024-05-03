0801f6af00315c6d7c38855ead62715d65925ade
Nageshwar Tripathi <stpandstp.martialstylus@gmail.com>
2024-05-03 22:15:35 +0530

    completed: StickerSmash

--- App.js
+++ App.js
@@ -139,7 +139,7 @@ export default function App() {
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </GestureHandlerRootView>
   );
 }
